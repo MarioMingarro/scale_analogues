e5487b480f4770caf1b0591399d48da946859dad
MarioMingarro <mario_mingarro@hotmail.com>
2022-05-25 14:28:43 +0200

    new

---
+++ Datalogguers_comparation.R
@@ -0,0 +1,55 @@
+library(readxl)
+tmed_datalogers <- read_excel("B:/MARIPOSAS/SCALE_ANALOGOUS/MICROCLIMA/DATALOGUERS/HABITATS_APOLLO/MATRIZ_COMPLETA_MICROCLIMA_APOLLO_CHELSA.xlsx", 
+                                                       sheet = "T_mean_dataloggers_mensual", 
+                              col_types = c("text", "text", "numeric", 
+                                            "numeric", "numeric", "numeric", 
+                                            "numeric", "numeric", "numeric", 
+                                            "numeric", "numeric", "numeric", 
+                                            "numeric", "numeric", "numeric", 
+                                            "numeric", "numeric", "numeric", 
+                                            "numeric", "numeric", "numeric", "numeric", "numeric", 
+                                            "numeric", "numeric", "numeric", "numeric", "numeric", 
+                                            "numeric", "numeric", "numeric"))
+
+tmed_chelsa <- read_excel("B:/MARIPOSAS/SCALE_ANALOGOUS/MICROCLIMA/DATALOGUERS/HABITATS_APOLLO/MATRIZ_COMPLETA_MICROCLIMA_APOLLO_CHELSA.xlsx", 
+                              sheet = "T_mean_chelsa_mensual")
+
+tmed_microclima <- read_excel("B:/MARIPOSAS/SCALE_ANALOGOUS/MICROCLIMA/DATALOGUERS/HABITATS_APOLLO/MATRIZ_COMPLETA_MICROCLIMA_APOLLO_CHELSA.xlsx", 
+                          sheet = "T_mean_microclima_mensual")
+
+
+averages <- as.data.frame(colMeans(tmed_datalogers[, 11:31], na.rm=TRUE))
+averages <- cbind(averages,colMeans(tmed_chelsa[, 11:31], na.rm=TRUE))
+averages <- cbind(averages,colMeans(tmed_microclima[, 11:31], na.rm=TRUE))
+averages <- cbind(averages,
+                  format(seq.Date(as.Date("2011-10-01"), as.Date("2013-06-01"), by = "month"),
+                         format = "%m-%Y"))
+averages <- cbind(averages,c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21))
+
+colnames(averages) <- c("Datalogguers", "Chelsa", "Microclima", "Date", "N")
+
+library(tidyverse)
+ggplot(averages)+
+  geom_point(aes(x= N, y= Datalogguers), col= "brown2")+
+  geom_smooth(aes(x=N, y= Datalogguers), col= "brown2", fill = "brown2", alpha = 0.2)+
+  geom_smooth(aes(x=N, y= Datalogguers), col= "brown2",method = lm, se = FALSE)+
+  geom_point(aes(x= N, y= Chelsa), col= "deepskyblue2")+
+  geom_smooth(aes(x=N, y= Chelsa), col= "deepskyblue2", fill = "deepskyblue2", alpha = 0.2)+
+  geom_smooth(aes(x=N, y= Chelsa), col= "deepskyblue2",method = lm, se = FALSE)+
+  geom_point(aes(x= N, y= Microclima), col= "chartreuse4")+
+  geom_smooth(aes(x=N, y= Microclima), col= "chartreuse4", fill = "chartreuse4", alpha = 0.2)+
+  geom_smooth(aes(x=N, y= Microclima), col= "chartreuse4",method = lm, se = FALSE)+
+  labs(y = "Temperature (ºC)", x = "Date")+
+  theme(
+    panel.background = element_rect(fill = "white",
+                                    colour = "white",
+                                    size = 0.5, linetype = "solid"),
+    axis.line = element_line(colour = "black"),
+    axis.text = element_text(family = "Courier", colour = "black", size = 10),
+    axis.title = element_text(family = "Courier", colour = "black", size = 10),
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank()
+  )
+
+
